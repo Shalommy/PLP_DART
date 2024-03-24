3ab421f111aedbbe4269897980aca14dca77e085
Shalommy <160464893+Shalommy@users.noreply.github.com>
2024-03-24 10:39:28 +0000

    Add files via upload

---
+++ W3Control_Flow.dart
@@ -0,0 +1,30 @@
+//Number
+var employeeID = 121220;
+
+//String
+String firstName = 'Nora';
+
+//String
+String lastName = 'James';
+
+//Boolean
+bool isEmployeePermanent = true;
+
+//DateTime
+var dateOfEmployment = DateTime(2020, 10, 20);
+
+//list
+var managers = ['Paul Moroka', 'Karabo Marumo'];
+
+//Maps
+var profession = {'Department': 'Marketing', 'salaryPerMonth': 'R20. 000'};
+
+void main() {
+  print("Employee ID: $employeeID");
+  print("First Name: $firstName");
+  print("Last Name: $lastName");
+  print("Is Employee Permanent: $isEmployeePermanent");
+  print("Date of Employment: $dateOfEmployment");
+  print("Managers: $managers");
+  print("Profession: $profession");
+}
